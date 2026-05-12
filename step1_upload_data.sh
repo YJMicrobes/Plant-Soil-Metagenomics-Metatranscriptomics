510361b33324d7c3adb92121ab80a7e52364d1e1
Jing Yuan <yuanj1203@outlook.com>
2026-05-12 16:00:17 -0400

    Update step1_upload_data.sh

--- step1_upload_data.sh
+++ step1_upload_data.sh
@@ -4,28 +4,28 @@
 ##############################################################################
 #login in pulse secure (vpn)
 #open local terminal 
-ssh username@mantis-submit.cam.uchc.edu
+ssh username@servername
 
 #if need the public key setup to login in, check here:
 https://cbc-uconn.github.io/hpc-docs/connect.html#ssh-keys
 #follow the recipe and then open the terminal to login in
 #either
-ssh username@login.hpc.cam.uchc.edu
+ssh username@servername
 #or 
-ssh username@mantis-submit.cam.uchc.edu
+ssh username@servername
 
 #upload
-#scp pod5/* username@mantis-submit.cam.uchc.edu:/home/FCAM/username/
-scp pod5/*.pod5 username@mantis-submit.cam.uchc.edu:/scratch/corn20260512/pod5
+#scp pod5/* username@servername:/home/FCAM/username/
+scp pod5/*.pod5 username@servername:/scratch/corn20260512/pod5
 
 
 #download
-scp username@mantis-submit.cam.uchc.edu:/home/FCAM/username/filename ~/Desktop 
+scp username@servername:/home/FCAM/username/filename ~/Desktop 
 
 #rsync -av --ignore-existing release220/ /scratch/bsteven/gtdb_database/
 
 #or use fillzella
-#transfer.cam.uchc.edu
+#servername
 #usename
 #code
 #port: 22
